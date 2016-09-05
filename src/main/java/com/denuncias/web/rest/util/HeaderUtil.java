b4e83bbbf726b4d8397df77f6a81b15c5c386e26
Juan Gabriel Guzman <jguzman@ioet.com>
2016-09-05 13:02:04 -0500

    Cambiado mensajes de creación y actualización de cantones

--- src/main/java/com/denuncias/web/rest/util/HeaderUtil.java
+++ src/main/java/com/denuncias/web/rest/util/HeaderUtil.java
@@ -16,15 +16,15 @@ public class HeaderUtil {
     }
 
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
-        return createAlert("A new " + entityName + " is created with identifier " + param, param);
+        return createAlert("Un nuevo " + entityName + " ha sido creado con el identificador " + param, param);
     }
 
     public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
-        return createAlert("A " + entityName + " is updated with identifier " + param, param);
+        return createAlert("El " + entityName + " está actualizado con el identificador" + param, param);
     }
 
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
-        return createAlert("A " + entityName + " is deleted with identifier " + param, param);
+        return createAlert("El " + entityName + " está eliminado con el identificador" + param, param);
     }
 
     public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
