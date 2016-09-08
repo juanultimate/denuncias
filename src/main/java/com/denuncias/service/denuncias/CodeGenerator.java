f0ddc492028a240f03ac4dd293f99c991cd83678
Juan Gabriel Guzmán <juanultimate@hotmail.com>
2016-09-07 22:37:47 -0500

    Modificado generador de codigo de la alerta

--- src/main/java/com/denuncias/service/denuncias/CodeGenerator.java
+++ src/main/java/com/denuncias/service/denuncias/CodeGenerator.java
@@ -4,6 +4,8 @@ import com.denuncias.repository.DenunciaRepository;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 @Component
 public class CodeGenerator {
@@ -11,6 +13,11 @@ public class CodeGenerator {
     @Inject
     private DenunciaRepository denunciaRepository;
     public String generarCodigo(){
-        return "AME".concat(String.valueOf(denunciaRepository.count()));
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        StringBuilder codigo = new StringBuilder("ACT-");
+        codigo.append(year).append(String.format("%02d", month)).append(String.format("%03d", denunciaRepository.count()));
+        return codigo.toString();
     }
 }
